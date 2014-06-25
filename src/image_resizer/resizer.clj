f96f01c9a19601fb4da6eb8516caa3f2a751db96
stlk <josef.rousek@gmail.com>
2014-06-25 11:05:07 +0200

    We're making images smaller not bigger

--- src/image_resizer/resizer.clj
+++ src/image_resizer/resizer.clj
@@ -6,7 +6,7 @@
            java.awt.geom.AffineTransform
            javax.imageio.ImageIO))
 
-(def gallery-path "/home/stlk/Documents/clojure/image-resizer/images/")
+(def gallery-path "/home/stlk/Documents/clojure/image-resizer/images/rental/")
 
 (defn scale [img ratio width height]
   (let [scale        (AffineTransform/getScaleInstance
@@ -20,14 +20,22 @@
         img-width  (.getWidth img)
         img-height (.getHeight img)
         ratio      (/ thumb-size img-height)]
-    (scale img ratio (int (* img-width ratio)) thumb-size)))
+    (if (< ratio 1)
+      (scale img ratio (int (* img-width ratio)) thumb-size)
+      img)))
 
-(defn save-thumbnail [dir filename thumb-size thumb-prefix]
-  (let [path (str gallery-path File/separator dir File/separator)]
+(defn save-thumbnail [dir filename new-filename thumb-size thumb-prefix]
+  (let [path (str dir File/separator)]
     (ImageIO/write
       (scale-image (io/input-stream (str path filename)) thumb-size)
       "jpeg"
-      (File. (str path thumb-prefix filename)))))
+      (File. (str gallery-path thumb-prefix new-filename)))))
+
+(defn resize-image [dir orig-path path]
+  (.mkdir (java.io.File. "/home/stlk/Documents/clojure/image-resizer/images/"))
+  (.mkdir (java.io.File. gallery-path))
+  (save-thumbnail dir orig-path path 1280 "")
+  (save-thumbnail dir orig-path path 180 "_"))
 
 
-(save-thumbnail "soucasnost" "Škoda Oct policie 5 dv. 005.jpg" 1280 "_")
+;(resize-image "/home/stlk/Documents/clojure/image-resizer/images_src/20-30/" "Hupmobile A 1928  USA.jpg" "Hupmobile A 1928  USA_xxx.jpg")
