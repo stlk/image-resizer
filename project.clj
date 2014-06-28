d7a5df5a8a0c85d7e9047e6cc4dfb13d9e5377b2
stlk <josef.rousek@gmail.com>
2014-06-28 09:23:00 +0200

    Letting the external library do the resizing

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [selmer "0.6.8"]])
+                 [selmer "0.6.8"]
+                 [fivetonine/collage "0.2.0"]])
--- src/image_resizer/resizer.clj
+++ src/image_resizer/resizer.clj
@@ -1,41 +1,22 @@
 (ns image-resizer.resizer
-  (:require [clojure.java.io :as io])
-  (:import [java.io File FileInputStream FileOutputStream]
-           [java.awt.image AffineTransformOp BufferedImage]
-           java.awt.RenderingHints
-           java.awt.geom.AffineTransform
-           javax.imageio.ImageIO))
+  (:require [clojure.java.io :as io]
+            [fivetonine.collage.util :as util]
+            [fivetonine.collage.core :refer [with-image resize]])
+  (:import [java.io File]))
 
 (def gallery-path "/home/stlk/Documents/clojure/image-resizer/images/rental/")
 
-(defn scale [img ratio width height]
-  (let [scale        (AffineTransform/getScaleInstance
-                       (double ratio) (double ratio))
-        transform-op (AffineTransformOp.
-                       scale AffineTransformOp/TYPE_BILINEAR)]
-    (.filter transform-op img (BufferedImage. width height (.getType img)))))
-
-(defn scale-image [file thumb-size]
-  (let [img        (ImageIO/read file)
-        img-width  (.getWidth img)
-        img-height (.getHeight img)
-        ratio      (/ thumb-size img-height)]
-    (if (< ratio 1)
-      (scale img ratio (int (* img-width ratio)) thumb-size)
-      img)))
-
 (defn save-thumbnail [dir filename new-filename thumb-size thumb-prefix]
   (let [path (str dir File/separator)]
-    (ImageIO/write
-      (scale-image (io/input-stream (str path filename)) thumb-size)
-      "jpeg"
-      (File. (str gallery-path thumb-prefix new-filename)))))
+    (def image (util/load-image (str path filename)))
+    (with-image image
+            (resize :width thumb-size)
+            (util/save (str gallery-path thumb-prefix new-filename) :quality 1.0))))
 
 (defn resize-image [dir orig-path path]
-  (.mkdir (java.io.File. "/home/stlk/Documents/clojure/image-resizer/images/"))
-  (.mkdir (java.io.File. gallery-path))
+  (.mkdir (File. "/home/stlk/Documents/clojure/image-resizer/images/"))
+  (.mkdir (File. gallery-path))
   (save-thumbnail dir orig-path path 1280 "")
   (save-thumbnail dir orig-path path 180 "_"))
-
 
 ;(resize-image "/home/stlk/Documents/clojure/image-resizer/images_src/20-30/" "Hupmobile A 1928  USA.jpg" "Hupmobile A 1928  USA_xxx.jpg")
